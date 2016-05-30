7e0a8c5ff9ec9289a7fee18cefdc3c32c3bc65e2
Renan Martins <email": null>
2016-05-30 15:35:15 -0700

    Add myPost variable to MainCtrl and add upvotePost function

--- share-your-thoughts/app/assets/javascripts/home/mainCtrl.js
+++ share-your-thoughts/app/assets/javascripts/home/mainCtrl.js
@@ -5,15 +5,26 @@ angular.module('shareYourThoughts')
   // Associate posts variable with the posts in the factory object
   $scope.posts = postsFactory.posts;
 
+  // For the form post information
+  $scope.myPost = { content: "", upvotes: 0 };
+
   // Function to add a new post (through the HTML form)
   $scope.addPost = function() {
     console.log($scope.postForm.$valid);
 
     // Create the post (also works on the backend)
-    postsFactory.create({ content: $scope.content });
+    //postsFactory.create({ content: $scope.content });
+    postsFactory.create($scope.myPost);
 
     // Clear the form
     $scope.postForm.$setPristine();
+
+    // Reset the variable bound to the form input
+    $scope.myPost = { content: "", upvotes: 0 };
+  };
+
+  $scope.upvotePost = function(post) {
+    postsFactory.upvote(post);
   };
 
 }]);
