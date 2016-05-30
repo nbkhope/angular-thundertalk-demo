3fafcbfab7e219428794826915ba41ef364a63d6
Renan Martins <email": null>
2016-05-30 10:02:56 -0700

    Add get and create methods for postsFactory

--- share-your-thoughts/app/assets/javascripts/posts/posts.js
+++ share-your-thoughts/app/assets/javascripts/posts/posts.js
@@ -13,6 +13,18 @@ angular.module('shareYourThoughts')
     });
   };
 
+  o.get = function(id) {
+    return $http.get('/posts/' + id + '.json').then(function(res) {
+      return res.data;
+    });
+  };
+
+  o.create = function(post) {
+    return $http.post('/posts.json', post).success(function(data) {
+      o.posts.push(data);
+    });
+  };
+
   // return the factory object
   return o;
 
