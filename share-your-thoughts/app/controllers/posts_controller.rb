0dbfe8e6a479b44ec05fb87e7d0f5cb9e35eb1b7
Renan Martins <email": null>
2016-05-30 09:59:28 -0700

    Fix arguments to merge method in post_params in PostsController

--- share-your-thoughts/app/controllers/posts_controller.rb
+++ share-your-thoughts/app/controllers/posts_controller.rb
@@ -21,6 +21,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content).merge(:upvotes: 0)
+    params.require(:post).permit(:content).merge(upvotes: 0)
   end
 end
