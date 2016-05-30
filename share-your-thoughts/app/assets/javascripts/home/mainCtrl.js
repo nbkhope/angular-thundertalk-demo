e0296766a0f55ede8ae5be8173de53e5deef30c2
Renan Martins <email": null>
2016-05-30 14:59:43 -0700

    Add addPost function to MainCtrl

--- share-your-thoughts/app/assets/javascripts/home/mainCtrl.js
+++ share-your-thoughts/app/assets/javascripts/home/mainCtrl.js
@@ -2,6 +2,18 @@ angular.module('shareYourThoughts')
 
 .controller('MainCtrl', ['$scope', 'postsFactory', function($scope, postsFactory) {
 
+  // Associate posts variable with the posts in the factory object
   $scope.posts = postsFactory.posts;
+
+  // Function to add a new post (through the HTML form)
+  $scope.addPost = function() {
+    console.log($scope.postForm.$valid);
+
+    // Create the post (also works on the backend)
+    postsFactory.create({ content: $scope.content });
+
+    // Clear the form
+    $scope.postForm.$setPristine();
+  };
 
 }]);
