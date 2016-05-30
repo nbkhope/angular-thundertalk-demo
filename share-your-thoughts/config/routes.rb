9310f3990b0aa35826b3a0411d953ab24670db16
Renan Martins <email": null>
2016-05-30 09:30:03 -0700

    Add RESTful routes for posts along with member put '/upvote' for post

--- share-your-thoughts/config/routes.rb
+++ share-your-thoughts/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # Define the home page
   root to: 'application#angular'
+
+  resources :posts do
+    # Using member makes the route use :id instead of :post_id (/posts/:id/upvote)
+    member do
+      put '/upvote' => 'posts#upvote'
+    end
+  end
 end
